49dea456295ab59994754b0b4e6ad0e415413d26
mahamudhul <111944934+mahamudhul@users.noreply.github.com>
2023-05-19 02:54:10 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-center m-10'>Hello world</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Header.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-slate-900	 text-white">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold">
+                            <Link to='/'>Home</Link>
+                            <Link to='/alltoys'>All toys</Link>
+                            <Link to='/addtoys'>Add toys</Link>
+                            <Link to='/blogs'>Blogs</Link>
+                            <Link to='/login'>Log in</Link>
+                            <Link to='/register'>Register</Link>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-5 font-bold">
+                        <Link to='/'>Home</Link>
+                        <Link to='/alltoys'>All toys</Link>
+                        <Link to='/addtoys'>Add toys</Link>
+                        <Link to='/blogs'>Blogs</Link>
+                        <Link to='/login'>Log in</Link>
+                        <Link to='/register'>Register</Link>
+                    </ul>
+                </div>
+                <div className="navbar-end ">
+                    <div className="w-10 rounded-full">
+                        <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
