fc5153e39aad7c800dacd5447acd5efdfacb1026
mahamudhul <111944934+mahamudhul@users.noreply.github.com>
2023-05-19 02:56:14 +0600

    style main body and footer file  added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Header from '../pages/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Footer';
 
 const Main = () => {
     return (
-        <div>
+        <div className='my-12 mx-12 shadow-lg rounded-lg'>
             <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
